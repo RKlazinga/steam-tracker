fda844f0b49b0bdea0c4e2a1e621bed82277b89b
Andor Michels <andor.michels@redian.nl>
2022-03-24 18:59:49 +0100

    Add data saving

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import math
 import json
 import os.path
+import pickle
 import re
 import datetime
+from collections import defaultdict
+from datetime import datetime as dt
+
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
@@ -13,7 +17,7 @@ def get_data():
     file_names = ["content_log.previous.txt", "content_log.txt"]
     p = re.compile('\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] AppID \d+ state changed : (\w| |,)+')
     last_times = dict()
-    play_data = dict()
+    play_data = defaultdict(list)
     for file_name in file_names:
         f = open(file_location + "\\" + file_name)
         for line in f:
@@ -23,14 +27,13 @@ def get_data():
                 game_id = int(line.split()[3])
 
                 if game_id in last_times:
-                    if game_id in play_data:
-                        play_data[game_id].append({"start": last_times[game_id], "end": date})
-                    else:
-                        play_data[game_id] = [{"start": last_times[game_id], "end": date}]
+                    play_data[game_id].append((int(dt.timestamp(last_times[game_id])),
+                                               int(dt.timestamp(date))))
                     del last_times[game_id]
                 elif start_game:
                     last_times[game_id] = date
     return play_data
+
 
 def get_data_step_size(max_val):
     if max_val < 60:
@@ -41,6 +44,7 @@ def get_data_step_size(max_val):
         return 30
     return 60
 
+
 def plot_stats(data, game_names, days):
     fig = plt.figure()
     ax = fig.add_axes([0.1, 0.15, 0.8, 0.75])
@@ -48,7 +52,7 @@ def plot_stats(data, game_names, days):
     plt.plot([*zip(*data)])
 
     y_step_size = get_data_step_size(np.amax(data))
-    plt.yticks(np.arange(0, math.ceil(np.amax(data)/y_step_size+1)*y_step_size, y_step_size))
+    plt.yticks(np.arange(0, math.ceil(np.amax(data) / y_step_size + 1) * y_step_size, y_step_size))
     plt.xticks(range(len(days)), [day.strftime("%Y-%m-%d") for day in days], rotation=30, ha="right")
 
     plt.grid()
@@ -58,9 +62,10 @@ def plot_stats(data, game_names, days):
 
 
 def get_day_span(game_stats):
-    min_date = min([min([time['start'] for time in item]) for item in game_stats.values()])
-    max_date = max([max([time['end'] for time in item]) for item in game_stats.values()])
-    return min_date, max_date
+    min_date = min([time[0] for item in game_stats.values() for time in item])
+    max_date = max([time[1] for item in game_stats.values() for time in item])
+    return dt.fromtimestamp(min_date), \
+           dt.fromtimestamp(max_date)
 
 
 def gen_dates(min_date, max_date):
@@ -78,9 +83,9 @@ def gen_time_stats(game_stats):
 
     for game_id in game_stats:
         for time in game_stats[game_id]:
-            date_index = time['start'].toordinal() - min_date.toordinal()
+            date_index = dt.fromtimestamp(time[0]).toordinal() - min_date.toordinal()
             game_index = game_ids.index(game_id)
-            game_time = (time['end'] - time['start']).total_seconds() / 60
+            game_time = (time[1] - time[0]) / 60
             data[game_index][date_index] += game_time
 
     return data, min_date, max_date
@@ -102,6 +107,23 @@ def get_game_names(game_ids):
         writefile.write(json.dumps(names))
     return names
 
+def save_data(play_data):
+    binary_data = pickle.dumps(play_data)
+    with open("play_data_cache", "wb") as writefile:
+        writefile.write(binary_data)
+
+def load_data():
+    with open("play_data_cache", "rb") as readfile:
+        binary_data = readfile.read()
+        play_data = pickle.loads(binary_data)
+        return play_data
+
+def merge_data(data1, data2):
+    # We will merge data2 into data1
+    for game_id in data2:
+        data1[game_id] = list(set(data1[game_id] + data2[game_id]))
+    return data1
+
 
 def main():
     if not os.path.isfile("name_cache"):
@@ -109,6 +131,10 @@ def main():
             writefile.write("{}")
 
     play_data = get_data()
+    cached_data = load_data()
+    play_data = merge_data(cached_data, play_data)
+    save_data(play_data)
+
     game_ids = list(play_data.keys())
     game_names = get_game_names(game_ids)
 
